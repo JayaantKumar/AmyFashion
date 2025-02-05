052f5b4ba88a5651c3fb6bf546166431c838ab1b
JayaantKumar <112800009+JayaantKumar@users.noreply.github.com>
2025-02-06 02:06:56 +0530

    added multer in productcontroller and created all the functionalities for product

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -7,8 +7,8 @@ const productSchema = new mongoose.Schema({
   image: { type: Array, required: true },
   category: { type: String, required: true },
   subCategory: { type: String, required: true },
-  sizes: { type: Number, required: true },
-  sizes: { type: Boolean },
+  sizes: { type: Array, required: true },
+  bestseller: { type: Boolean },
   date: { type: Number, required: true },
 });
 
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import connectDB from './config/mongobd.js'
 import connectCloudinary from './config/cloudinary.js'
 import userRouter from './Routes/userRoute.js'
+import productRouter from './Routes/productRoute.js'
 
 // App config
 const app = express()
@@ -17,6 +18,7 @@ app.use(cors())
 
 // api endpoints
 app.use('/api/user', userRouter)
+app.use('/api/product',productRouter)
 
 app.get('/',(req,res)=>{
     res.send("API Working")
